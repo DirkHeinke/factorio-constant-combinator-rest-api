48b684e0d2b1136cacbfc823e88a3113ea7cf842
Dirk Heinke <dirkheinke@gmail.com>
2022-05-08 00:10:54 +0200

    Add example and allows CORS

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,15 @@ const app = express();
 const factorio = new FactorioConnector();
 
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 
 app.get("/cc/:id/signals", async (req, res) => {
   const id = req.params.id;
